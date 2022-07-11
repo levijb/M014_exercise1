4ad30e646f833eff48a3e3334585f73fabdc37ce
Devin McDonald <devinmcdonald@MacBook-Pro.local>
2022-07-11 10:49:43 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Module 14',
+    version='1.0.0',
+    url='https://github.com/levijb/M014_exercise1.git',
+    author='Levi Davis and Devin McDonald',
+    author_email='ljd3frf@virginia.edu and djm6cz@virginia.edu',
+    description='M14 Class Challenge Package',
+    packages=find_packages(),    
+    install_requires=None,
+)
