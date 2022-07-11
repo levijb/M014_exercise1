b1a0d4f23591578c282d056f5bb7bd013d014529
levijb <levijbdavis@gmail.com>
2022-07-11 10:41:40 -0400

    module

---
+++ module.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 10:33:40 2022
+
+@author: ljd3frf
+"""
+
+def multiply(a, b): 
+    '''
+    This function takes two parameters, both numbers, and return their product.
+    '''
+    return a*b
+    
